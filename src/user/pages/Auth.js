c2f7bc5d6b4dbb332d3960e2722b5072af27bfa9
jimmy2952 <jimmy2952@gmail.com>
2020-11-14 12:59:16 +0800

    authentication

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -78,7 +78,7 @@ const Auth = (props) => {
           }),
           { "Content-Type": "application/json" }
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     } else {
       try {
@@ -93,7 +93,7 @@ const Auth = (props) => {
           formData,
         );
 
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     }
   };
